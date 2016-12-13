8e1094574d718d15d61176a2ed59fa230618ef16
Sun Bo <starry@vip.qq.com>
2016-12-13 13:46:38 +0800

    unit test for sql insert

--- Starry.Data.Tests/DbClientTest.cs
+++ Starry.Data.Tests/DbClientTest.cs
@@ -32,9 +32,50 @@ namespace Starry.Data.Tests
             var db = DbFixed.Instance.GetClient();
             var sqlText = @"
 SELECT *
-  FROM BLOGINFO";
+  FROM BLOGINFO
+";
             var result = db.Query<Models.BlogInfo>(sqlText);
             Assert.True(result != null && result.Any());
+        }
+
+        [Fact]
+        public void DbClientInsertTest()
+        {
+            var db = DbFixed.Instance.GetClient();
+            var blogInfo = new Models.BlogInfo();
+            {
+                var sqlText = @"
+INSERT INTO
+    BLOGINFO (
+        BITitle,
+        BIContent,
+        BICreateUser)
+    VALUES (
+        @BITitle,
+        @BIContent,
+        @BICreateUser);
+SELECT LAST_INSERT_ROWID()
+";
+                blogInfo.BITitle = Guid.NewGuid().ToString();
+                blogInfo.BIContent = Guid.NewGuid().ToString();
+                blogInfo.BICreateUser = new Random((int)(DateTime.Now.Ticks % int.MaxValue)).Next(0, 10000);
+                blogInfo.BIID = db.ExecuteScalar<int>(sqlText, blogInfo);
+                Assert.True(blogInfo.BIID > 0);
+            }
+            {
+                var sqlText = @"
+SELECT *
+  FROM BLOGINFO
+ WHERE BIID = @BIID
+";
+                var result = db.Query<Models.BlogInfo>(sqlText, new { blogInfo.BIID });
+                Assert.True(result != null && result.Any());
+                var info = result.First();
+                Assert.Equal(blogInfo.BIID, info.BIID);
+                Assert.Equal(blogInfo.BITitle, info.BITitle);
+                Assert.Equal(blogInfo.BIContent, info.BIContent);
+                Assert.Equal(blogInfo.BICreateUser, info.BICreateUser);
+            }
         }
     }
 }
